bed38e8216cc34a7a2e3a1ec7617cda5ee7045ed
Kibria92 <kibria92@gmail.com>
2020-08-14 01:27:07 +0600

    ok

--- src/main/java/com/bikash/bikashBackend/BikashBackendApplication.java
+++ src/main/java/com/bikash/bikashBackend/BikashBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BikashBackendApplication {
+public class BikashBackendApplication {//ok
 
 	public static void main(String[] args) {
 		SpringApplication.run(BikashBackendApplication.class, args);
