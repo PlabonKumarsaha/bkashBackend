d18b45684eb8f5c1c7c8ad6fbd69bf2d71935bb1
Kibria92 <kibria92@gmail.com>
2020-08-14 01:07:14 +0600

    In this repository will make backend project like Bikash

---
+++ src/main/java/com/bikash/bikashBackend/BikashBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bikash.bikashBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikashBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikashBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikash/bikashBackend/BikashBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikash.bikashBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikashBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
