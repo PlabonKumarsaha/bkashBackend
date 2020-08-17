fb8c2629657447709953ff8f7bcec2635c724cc9
Kibria92 <kibria92@gmail.com>
2020-08-17 22:24:36 +0600

    add some dependency

--- src/main/java/com/bikash/bikashBackend/BikashBackendApplication.java
+++ src/main/java/com/bikash/bikashBackend/BikashBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BikashBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BikashBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BikashBackendApplication.class, args);
+    }
 
 }
